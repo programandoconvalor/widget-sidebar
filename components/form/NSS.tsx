728934a75ac0c41f0c75191f1b3278567b3862bc
JC - FullStack Dev <94090604+programandoconvalor@users.noreply.github.com>
2023-03-13 15:27:15 -0600

    testing SRC1

--- components/form/NSS.tsx
+++ components/form/NSS.tsx
@@ -8,6 +8,7 @@ export default function NSS (props:any)  {
     <>
     {props.data.res.map((item:ResponseEnty, index:number) => (
       <div key={index}>
+        Pokemons
         <div>Name: {item.name}</div>
         {/*<div>URL: <Img src="https://th.bing.com/th/id/OIP.yVUuOcXPtPb3A7JmWNyb7wEsCW?pid=ImgDet&rs=1" alt="img" width={100} height={100}/></div>*/}
         <div>URL:{item.url}</div>
